722373c333039758bcef3077ce57659bf03039e3
ofey51 <domenuk10@gmail.com>
2022-10-18 17:13:49 +0300

    feat: add brain-prime game

---
+++ games/brain-prime.js
@@ -0,0 +1,30 @@
+import { greeting, answer } from '../src/index.js';
+
+const brainPrime = () => {
+  const userName = greeting();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  let correctAnswers = 0;
+  const numGenerator = () => Math.floor(Math.random() * 101);
+
+  while (correctAnswers < 3) {
+    const number = numGenerator();
+    let trueAnswer = true;
+    for (let i = 2; i <= Math.sqrt(number); i += 1) {
+      if (number % i === 0) {
+        trueAnswer = false;
+        break;
+      }
+    }
+    trueAnswer = trueAnswer === true ? 'yes' : 'no';
+    const question = number;
+    if (answer(question, trueAnswer, userName)) {
+      correctAnswers += 1;
+    } else break;
+  }
+  if (correctAnswers === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+export default brainPrime;
